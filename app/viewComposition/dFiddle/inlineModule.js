cbbaa621b43fb3a3a33f65233fe289501085c028
thelinuxlich <thelinuxlich@gmail.com>
2013-07-03 20:08:43 -0300

    Changing viewComposition example

--- app/viewComposition/dFiddle/inlineModule.js
+++ app/viewComposition/dFiddle/inlineModule.js
@@ -1,5 +1,8 @@
 define(function(){
     return {
-        someProperty:'This is being bound against an inline view.'
+        someProperty:'This is being bound against an inline view.',
+        activate: function() {
+            this.someProperty = "This was changed inside the activate function of the inlined module.";
+        }
     };
 });
