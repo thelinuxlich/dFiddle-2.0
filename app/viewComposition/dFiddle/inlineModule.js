5fee8f10b8990e5b0ddd9e0bf5b0ef87e8457674
thelinuxlich <thelinuxlich@gmail.com>
2013-07-04 15:29:08 -0300

    Adding a subscription

--- app/viewComposition/dFiddle/inlineModule.js
+++ app/viewComposition/dFiddle/inlineModule.js
@@ -1,6 +1,9 @@
 define(['knockout'],function(ko){
     var InlineModule = function() {
         this.someProperty = ko.observable('This is being bound against an inline view.');
+        this.someProperty.subscribe(function(){
+            console.log("value changed");
+        });
     };
     InlineModule.prototype.activate = function() {
         this.someProperty("This was changed inside the activate function of the inlined module with cacheViews: true.");
